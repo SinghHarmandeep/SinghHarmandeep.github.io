1c45a4637d6bab1ba253554794a09ba389642f8d
Harmandeep Singh <deepharman2000@gmail.com>
2020-05-25 21:20:31 -0400

    added dynamic background color, added paragraph content and js file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var curTime = new Date().getHours();
+//if day hoours do the following
+if (curTime >=7 && curTime <=19) {
+if (document.body) {
+        document.getElementById("body").style.backgroundColor = "white";
+        document.getElementById("p").style.color = "black"; 
+    }
+} else {
+    //if night hours
+    if (document.body) {
+        document.getElementById("body").style.backgroundColor = "black";
+        document.getElementById("p").style.color = "white";
+    }
+}
