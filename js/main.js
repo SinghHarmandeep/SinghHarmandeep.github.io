d4ce399518b2f6a26dd69a53930a62b254c1941c
Harmandeep Singh <deepharman2000@gmail.com>
2021-03-18 14:58:03 -0400

    simple change

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-var curTime = new Date().getHours();
-//if day hoours do the following
-if (curTime >=7 && curTime <=19) {
-if (document.body) {
-        document.getElementById("body").style.backgroundColor = "white";
-        document.getElementById("p").style.color = "black"; 
-    }
-} else {
-    //if night hours
-    if (document.body) {
-        document.getElementById("body").style.backgroundColor = "black";
-        document.getElementById("p").style.color = "white";
-    }
-}
+// var curTime = new Date().getHours();
+// //if day hoours do the following
+// if (curTime >=7 && curTime <=19) {
+// if (document.body) {
+//         document.getElementById("body").style.backgroundColor = "white";
+//         document.getElementById("p").style.color = "black"; 
+//     }
+// } else {
+//     //if night hours
+//     if (document.body) {
+//         document.getElementById("body").style.backgroundColor = "black";
+//         document.getElementById("p").style.color = "white";
+//     }
+// }
+
+$("body").after("afdsafafdaf");
